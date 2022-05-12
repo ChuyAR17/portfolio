aa86cf0f695c86c6978e9d6f767977aa6037447e
Jesus Ramirez <jar170396@gmail.com>
2022-05-12 10:27:36 -0700

    feat(Portfolio): Init config for the portfolio.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1>Jesus Ramirez Porfolio</h1>
+        <p>
+          Coming soon.
+        </p>
+      </header>
+    </div>
+  );
+}
