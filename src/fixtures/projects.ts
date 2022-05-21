7de33687cf4f63ce73e682107cf2013bf8581e3b
Jesus Ramirez <jar170396@gmail.com>
2022-05-21 13:54:45 -0700

    feat(App): Added new link to web projects.

--- src/fixtures/projects.ts
+++ src/fixtures/projects.ts
@@ -3,8 +3,15 @@ export const listProjects = [
     name: 'Gym Logs',
     imgUrl: 'https://t1.uc.ltmcdn.com/es/posts/5/8/6/cuantas_calorias_se_queman_en_el_gym_48685_orig.jpg',
     description: 'Web App that helps you to keep track of your gym progress.',
-    github: 'https://github.com/ChuyAR17/',
-    web: 'https://www.google.com/'
+    github: 'https://github.com/ChuyAR17/gym-logs',
+    web: 'https://gym-logs.vercel.app/'
+  },
+  {
+    name: 'E-Art Store',
+    imgUrl: 'https://www.stlawu.edu/sites/default/files/styles/1900x1045/public/2022-03/IMG_9409.jpg?h=19f14c2c&itok=GOYfK86M',
+    description: 'Its an e-comerce where you can find paints and digital drawings, all in one place. Everything is on sale.',
+    github: 'https://github.com/ChuyAR17/ea-store',
+    web: 'https://628950f234ec3d5143159b5d--eart-shop.netlify.app/'
   },
   {
     name: 'Snake Game',
